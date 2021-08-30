3d6ebc828f08fef9f5f04f41d56086d01c157d96
pstaal <peter.staal@gmail.com>
2021-08-30 14:24:39 +0200

    add authenticate method to user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,9 @@
 import client from "../database";
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+
+const pepper = process.env.BCRYPT_PASSWORD;
+const saltRounds = process.env.SALT_ROUNDS;
 
 export type User {
   id: Number,
@@ -38,14 +43,38 @@ export class UserStore {
     }
   }
 
+  async authenticate(firstName: string, lastName: string, password: string): Promise<User | null> {
+
+    const sql = 'SELECT password FROM users WHERE firstName=($1) AND lastName=($2)'
+    // @ts-ignore
+    const conn = await client.connect()
+
+    const result = await conn.query(sql, [firstName, lastName]);
+
+    conn.release();
+
+    if (result.rows.length) {
+      const user = result.rows[0];
+
+      if (bcrypt.compareSync(password + pepper, user.password)) {
+        return user
+      }
+    }
+    return null;
+  }
+
   async create(u: User): Promise<User> {
     try {
       const sql = 'INSERT INTO users (firstName, lastName, password) VALUES($1, $2, $3) RETURNING *'
       // @ts-ignore
-      const conn = await client.connect()
+      const conn = await client.connect();
+      const hash = bcrypt.hashSync(
+        u.password + pepper,
+        parseInt(saltRounds)
+      );
 
       const result = await conn
-        .query(sql, [u.firstName, u.lastName, u.password])
+        .query(sql, [u.firstName, u.lastName, hash])
 
       const user = result.rows[0]
 
