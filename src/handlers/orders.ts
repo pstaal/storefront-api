0cca1fe43084623904498cb4912cfae990190232
pstaal <peter.staal@gmail.com>
2021-08-30 12:32:21 +0200

    add order routes to order handler file

--- src/handlers/orders.ts
+++ src/handlers/orders.ts
@@ -2,3 +2,22 @@ import express, { Request, Response } from "express";
 import { Order, OrderStore } from "../models/order";
 
 const store = new OrderStore();
+
+
+const getCurrent = async (req: Request, res: Response) => {
+  const order = await store.currentOrderUser(req.params.id);
+  res.json(user);
+}
+
+const getCompleted = async (req: Request, res: Response) => {
+  const users = await store.completedOrderUser(req.params.id);
+  res.json(users);
+}
+
+const order_routes = (app: express.Application) => {
+  app.get('/currentorder/:id', getCurrent);
+  app.get('/completedorders/:id', getCompleted);
+}
+
+
+export default order_routes;
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
 import user_routes from './handlers/users';
 import product_routes from './handlers/products';
+import order_routes from './handlers/orders';
 
 const app: express.Application = express()
 const address: string = "0.0.0.0:3000"
@@ -14,6 +15,7 @@ app.get('/', function (req: Request, res: Response) {
 
 user_routes(app);
 product_routes(app);
+order_routes(app);
 
 app.listen(3000, function () {
     console.log(`starting app on: ${address}`)
