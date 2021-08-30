4eaa1bf85623d4f778cf77caa244b21ee086c2a8
pstaal <peter.staal@gmail.com>
2021-08-30 11:41:25 +0200

    add methods to order store

--- src/models/order.ts
+++ src/models/order.ts
@@ -11,6 +11,36 @@ export type Order {
 
 export class OrderStore {
 
+  async currentOrderUser(user_id: string): Promise<Order> {
+    try {
+      const sql = 'SELECT * FROM orders WHERE user_id=($1) AND status="active"'
+      // @ts-ignore
+      const conn = await client.connect()
 
+      const result = await conn.query(sql, [user_id])
+
+      conn.release()
+
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(`Could not find an order for user with id ${user_id}. Error: ${err}`)
+    }
+  }
+
+  async completedOrderUser(user_id: string): Promise<Order[]> {
+    try {
+      const sql = 'SELECT * FROM orders WHERE user_id=($1) AND status="complete"'
+      // @ts-ignore
+      const conn = await client.connect()
+
+      const result = await conn.query(sql, [user_id])
+
+      conn.release()
+
+      return result.rows;
+    } catch (err) {
+      throw new Error(`Could not find any completed orders for user with id ${user_id}. Error: ${err}`)
+    }
+  }
 
 };
