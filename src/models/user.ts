ce1035038c108533bb135fd6813c8c790d8f57c8
pstaal <peter.staal@gmail.com>
2021-08-30 11:08:05 +0200

    add index, show, create in user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -9,6 +9,52 @@ export type User {
 
 export class UserStore {
 
+  async index(): Promise<User[]> {
+    try {
+      const conn = await client.connect();
+      const sql = 'SELECT * FROM users';
+      const result = await conn.query(sql);
+      conn.release();
+      return result.rows;
+    }
+    catch (err) {
+      throw new Error(`Cannot get users ${err}`);
+    }
+  }
 
+  async show(id: string): Promise<User> {
+    try {
+      const sql = 'SELECT * FROM users WHERE id=($1)'
+      // @ts-ignore
+      const conn = await Client.connect()
+
+      const result = await conn.query(sql, [id])
+
+      conn.release()
+
+      return result.rows[0]
+    } catch (err) {
+      throw new Error(`Could not find user ${id}. Error: ${err}`)
+    }
+  }
+
+  async create(u: User): Promise<User> {
+    try {
+      const sql = 'INSERT INTO users (firstName, lastName, password) VALUES($1, $2, $3) RETURNING *'
+      // @ts-ignore
+      const conn = await Client.connect()
+
+      const result = await conn
+        .query(sql, [u.firstName, u.lastName, u.password])
+
+      const user = result.rows[0]
+
+      conn.release()
+
+      return user
+    } catch (err) {
+      throw new Error(`Could not add new user ${u.firstName}. Error: ${err}`)
+    }
+  }
 
 };
