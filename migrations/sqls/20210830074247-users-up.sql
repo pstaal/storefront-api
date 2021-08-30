e8e879b9928e6d6ceb41fb029ef02ed9919b43bd
pstaal <peter.staal@gmail.com>
2021-08-30 10:02:02 +0200

    add all up and down migrations for the database

--- migrations/sqls/20210830074247-users-up.sql
+++ migrations/sqls/20210830074247-users-up.sql
@@ -1,1 +1,6 @@
-/* Replace with your SQL commands */
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    firstName VARCHAR(80) NOT NULL,
+    lastName VARCHAR(80) NOT NULL,
+    password VARCHAR NOT NULL
+);
