c258028262ef34c04e34e7b65d68cdfa629dde57
pstaal <peter.staal@gmail.com>
2021-08-31 09:26:47 +0200

    add more tests

--- src/models/tests/order_spec.ts
+++ src/models/tests/order_spec.ts
@@ -5,8 +5,15 @@ const store = new OrderStore();
 
 describe("Order Model", () => {
   
+  it('should have an current order method', () => {
+    expect(store.currentOrderUser).toBeDefined();
+  });
+
+  it('should have a method that gives a list of completed orders', () => {
+    expect(store.completedOrderUser).toBeDefined();
+  });
 
 
 
-  
+
 });
