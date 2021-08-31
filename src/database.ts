71bb453da1a3668acd9f0f32b59c740ee6f7b278
pstaal <peter.staal@gmail.com>
2021-08-31 09:04:35 +0200

    install jasmine

--- src/database.ts
+++ src/database.ts
@@ -7,15 +7,29 @@ const {
   POSTGRES_HOST,
   POSTGRES_DB,
   POSTGRES_USER,
-  POSTGRES_PASSWORD
+  POSTGRES_PASSWORD,
+  POSTGRES_TEST_DB,
+  ENV
 } = process.env;
 
+let client;
 
-const client = new Pool({
-  host: POSTGRES_HOST,
-  database: POSTGRES_DB,
-  user: POSTGRES_USER,
-  password: POSTGRES_PASSWORD
-});
+if(ENV === 'test') {
+  client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_TEST_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+  })
+}
+
+if(ENV === 'dev') {
+  client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+  })
+}
 
 export default client;
