1511051b23ecb643502191bb13965391350ee541
pstaal <peter.staal@gmail.com>
2021-09-01 13:55:14 +0200

    debugging

--- src/database.ts
+++ src/database.ts
@@ -17,7 +17,7 @@ let client!: Pool
 if(ENV === 'test') {
   client = new Pool({
     host: POSTGRES_HOST,
-    database: POSTGRES_DB,
+    database: POSTGRES_TEST_DB,
     user: POSTGRES_USER,
     password: POSTGRES_PASSWORD,
   })
@@ -26,7 +26,7 @@ if(ENV === 'test') {
 if(ENV === 'dev') {
   client = new Pool({
     host: POSTGRES_HOST,
-    database: POSTGRES_TEST_DB,
+    database: POSTGRES_DB,
     user: POSTGRES_USER,
     password: POSTGRES_PASSWORD,
   })
