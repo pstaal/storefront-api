1bb028ceea1072af8db62a170a3c8b5516327a3e
pstaal <peter.staal@gmail.com>
2021-09-07 13:59:45 +0200

    debugging

--- src/services/dashboard.ts
+++ src/services/dashboard.ts
@@ -14,7 +14,7 @@ export class DashboardQueries {
     try {
       //@ts-ignore
       const conn = await client.connect()
-      const sql = 'SELECT p.id, p.name, p.category, SUM(op.quantity) volume,COUNT(op.order_id) orders_placed FROM (products p INNER JOIN orders_products op on p.id=op.product_id) GROUP BY p.id ORDER BY volume DESC LIMIT 5'
+      const sql = 'SELECT p.id, p.name, p.category, SUM(op.quantity) volume,COUNT(op.order_id) orders_placed FROM (products p INNER JOIN order_products op on p.id=op.product_id) GROUP BY p.id ORDER BY volume DESC LIMIT 5'
 
       const result = await conn.query(sql)
 
