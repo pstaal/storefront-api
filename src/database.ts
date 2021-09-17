cc9ccfe993408c739c1e55a2ed21454934abf43c
pstaal <peter.staal@gmail.com>
2021-09-17 11:14:15 +0200

    change database.ts

--- src/database.ts
+++ src/database.ts
@@ -12,29 +12,36 @@ const {
   ENV
 } = process.env;
 
-let client!: Pool 
+// let client!: Pool 
 
 console.log(ENV==="test");
 
 console.log(ENV);
 
-if(ENV === 'test') {
-  client = new Pool({
-    host: POSTGRES_HOST,
-    database: POSTGRES_TEST_DB,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD,
-  })
-}
+const client: Pool = new Pool({
+  host: POSTGRES_HOST,
+  database: ENV === "dev" ? POSTGRES_DB : POSTGRES_TEST_DB,
+  user: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+});
 
-if(ENV === 'dev') {
-  client = new Pool({
-    host: POSTGRES_HOST,
-    database: POSTGRES_DB,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD,
-  })
-}
+// if(ENV === 'test') {
+//   client = new Pool({
+//     host: POSTGRES_HOST,
+//     database: POSTGRES_TEST_DB,
+//     user: POSTGRES_USER,
+//     password: POSTGRES_PASSWORD,
+//   })
+// }
+
+// if(ENV === 'dev') {
+//   client = new Pool({
+//     host: POSTGRES_HOST,
+//     database: POSTGRES_DB,
+//     user: POSTGRES_USER,
+//     password: POSTGRES_PASSWORD,
+//   })
+// }
 
 console.log("this is the client", client);
 
